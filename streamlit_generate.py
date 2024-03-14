1080630a5c3445a65c88d8bd3f9bdbed672f3d27
ge-al <148001595+ge-al@users.noreply.github.com>
2024-03-13 20:58:06 -0700

    Update streamlit_generate.py

--- streamlit_generate.py
+++ streamlit_generate.py
@@ -54,7 +54,7 @@ def generate_melody(chain, start_bigram):
 
 st.markdown('# Generate a National Anthem')
 st.markdown('##### George Altshuler & Hollie Zheng')
-st.markdown('###### ..........................................................................................')
+st.markdown('###### ......................................................................')
 
 
 df = pd.read_csv('anthems.csv')
@@ -76,7 +76,7 @@ if st.button('Generate Anthem Lyrics'):
 st.write(st.session_state.lyrics)
 
 
-st.markdown('###### ..........................................................................................')
+st.markdown('###### ......................................................................')
 
 
 st.subheader('National Anthem Melody Generator')
